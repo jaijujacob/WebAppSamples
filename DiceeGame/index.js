1c09a71a3d104fa0657dc55827585413ebee96bd
Jaiju Jacob <jaijujacob@gmail.com>
2020-05-08 14:14:29 +0530

    Adding dice play on click

--- DiceeGame/index.js
+++ DiceeGame/index.js
@@ -1,29 +1,27 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+function generateRandom(maxNum) {
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  return Math.floor(Math.random() * maxNum) + 1
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+
+function rollDicee() {
+  var randomNumber1 = generateRandom(6); //1-6
+  var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+  var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+  var image1 = document.querySelectorAll("img")[0];
+  image1.setAttribute("src", randomImageSource);
+  var randomNumber2 = generateRandom(6);
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+  //If player 1 wins
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 }
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
+

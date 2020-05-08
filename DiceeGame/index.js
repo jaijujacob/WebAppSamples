36d098546ca7eadf2507d50ae4317182f90e2d1b
Jaiju Jacob <jaijujacob@gmail.com>
2020-05-08 20:19:04 +0530

    Dice game modifications

--- DiceeGame/index.js
+++ DiceeGame/index.js
@@ -1,3 +1,5 @@
+var triesCount, playerOneTotal, playerTwoTotal;
+resetGame();
 
 function generateRandom(maxNum) {
 
@@ -5,19 +7,41 @@ function generateRandom(maxNum) {
 }
 
 function rollDicee() {
-  var randomNumber1 = generateRandom(6); //1-6
-  var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+  var randomNumberfirst = generateRandom(6); //1-6
+  var randomDiceImage = "dice" + randomNumberfirst + ".png"; //dice1.png - dice6.png
   var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
   var image1 = document.querySelectorAll("img")[0];
   image1.setAttribute("src", randomImageSource);
-  var randomNumber2 = generateRandom(6);
-  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  var randomNumberSecond = generateRandom(6);
+  var randomImageSource2 = "images/dice" + randomNumberSecond + ".png";
+
   document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-  //If player 1 wins
-  if (randomNumber1 > randomNumber2) {
+  if (triesCount === 10) {
+    //If player 1 wins
+    displayResults(playerOneTotal, playerTwoTotal);
+    resetGame();
+
+  }
+  else {
+    triesCount++;
+    playerOneTotal += randomNumberfirst;
+    playerTwoTotal += randomNumberSecond;
+    document.querySelector(".playerOne").innerHTML = "Player One : " + playerOneTotal;
+    document.querySelector(".playerTwo").innerHTML = "Player One : " + playerTwoTotal;
+  }
+}
+
+function resetGame() {
+  triesCount = 0;
+  playerOneTotal = 0;
+  playerTwoTotal = 0;
+}
+
+function displayResults(playerOneTotal, playerTwoTotal) {
+  if (playerOneTotal > playerTwoTotal) {
     document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
   }
-  else if (randomNumber2 > randomNumber1) {
+  else if (playerTwoTotal > playerOneTotal) {
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
   }
   else {

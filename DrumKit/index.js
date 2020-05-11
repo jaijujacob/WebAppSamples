b716221d654abed15476e9a8eacc7c7938856f6c
Jaiju Jacob <jaijujacob@gmail.com>
2020-05-11 22:50:24 +0530

    Drumkit finished code

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -2,42 +2,56 @@ var buttons = document.querySelectorAll("button");
 console.log();
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function () {
-        switch (this.innerHTML) {
-            case 'w':
-                var audio = new Audio('./sounds/tom-1.mp3');
-                audio.play();
-                break;
-            case 'a':
-                var audio = new Audio('./sounds/tom-2.mp3');
-                audio.play();
-                break;
-            case 's':
-                var audio = new Audio('./sounds/tom-3.mp3');
-                audio.play();
-                break;
-            case 'd':
-                var audio = new Audio('./sounds/tom-4.mp3');
-                audio.play();
-                break;
-
-            case 'j':
-                var audio = new Audio('./sounds/snare.mp3');
-                audio.play();
-                break;
-
-            case 'k':
-                var audio = new Audio('./sounds/kick-bass.mp3');
-                audio.play();
-                break;
-            case 'l':
-                var audio = new Audio('./sounds/crash.mp3');
-                audio.play();
-                break;
-
-
-            default:
-                break;
-        }
+        playSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
     })
 
+}
+
+document.addEventListener("keydown", function(){
+    console.log(event.key);
+    playSound(event.key);
+    buttonAnimation(event.key)
+})
+
+function playSound(key) {
+    switch (key) {
+        case 'w':
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(key){
+    document.querySelector("." + key).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("." + key).classList.remove("pressed");
+    }, 100);
 }
